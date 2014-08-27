a8a1a2de1e8451ffa4fbbe9fcc4b5562e0cb1112
Babkov Dmitriy <dsbabkov@gmail.com>
2014-08-27 16:59:57 +0400

    chapter05 tmp2

--- chapter05/spinnycube.cpp
+++ chapter05/spinnycube.cpp
@@ -4,25 +4,34 @@
 #include <QOpenGLVertexArrayObject>
 #include <QMatrix4x4>
 #include <QDebug>
+#include <QTime>
 
+#include <math.h>
 SpinnyCube::SpinnyCube(QWidget *parent) :
     QGLWidget(parent)
 {
     setWindowTitle("Spinny Cube");
+
     proj_matrix = new QMatrix4x4;
-    qDebug() << proj_matrix->isIdentity();
+    program = new QOpenGLShaderProgram;
+    time = new QTime;
+    time->start();
+    startTimer(16);
 }
 SpinnyCube::~SpinnyCube()
 {
     delete proj_matrix;
+    delete time;
 }
 
 void SpinnyCube::initializeGL()
 {
+    initializeOpenGLFunctions();
     glClearColor(0.0f, 0.25f, 0.0f, 1.0f);
     glClearDepth(1.0f);
 
     const char * vs_source =
+    {
         "#version 410 core                                                  \n"
         "                                                                   \n"
         "in vec4 position;                                                  \n"
@@ -39,9 +48,11 @@ void SpinnyCube::initializeGL()
         "{                                                                  \n"
         "    gl_Position = proj_matrix * mv_matrix * position;              \n"
         "    vs_out.color = position * 2.0 + vec4(0.5, 0.5, 0.5, 0.0);      \n"
-        "}                                                                  \n";
+        "}                                                                  \n"
+    };
 
     const char * fs_source =
+    {
         "#version 410 core                                                  \n"
         "                                                                   \n"
         "out vec4 color;                                                    \n"
@@ -54,9 +65,8 @@ void SpinnyCube::initializeGL()
         "void main(void)                                                    \n"
         "{                                                                  \n"
         "    color = fs_in.color;                                           \n"
-        "}                                                                  \n";
-
-    program = new QOpenGLShaderProgram(this);
+        "}                                                                  \n"
+    };
 
     program->addShaderFromSourceCode(QOpenGLShader::Fragment, fs_source);
     program->addShaderFromSourceCode(QOpenGLShader::Vertex, vs_source);
@@ -64,14 +74,13 @@ void SpinnyCube::initializeGL()
 
     mv_location = program->uniformLocation("mv_matrix");
     proj_location = program->uniformLocation("proj_matrix");
+//    mv_location = glGetUniformLocation(program, "mv_matrix");
+//    proj_location = glGetUniformLocation(program, "proj_matrix");
 
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-
-    QOpenGLVertexArrayObject* vao = new QOpenGLVertexArrayObject(this);
-    vao->create();
-    vao->bind();
-
-    const GLfloat vertex_positions[] =
+    static const GLfloat vertex_positions[] =
     {
         -0.25f,  0.25f, -0.25f,
         -0.25f, -0.25f, -0.25f,
@@ -122,14 +131,14 @@ void SpinnyCube::initializeGL()
         -0.25f,  0.25f, -0.25f
     };
 
-    QOpenGLBuffer* buffer = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
-    buffer->create();
-    qDebug() << "Buffer is bond: " << buffer->bind();
-    buffer->setUsagePattern(QOpenGLBuffer::StaticDraw);
-    buffer->allocate(vertex_positions, sizeof(vertex_positions));
-
-    program->setAttributeArray(0, GL_FLOAT, 0, 3);
-    program->enableAttributeArray(0);
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER,
+                 sizeof(vertex_positions),
+                 vertex_positions,
+                 GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
 
     glEnable(GL_CULL_FACE);
     glFrontFace(GL_CW);
@@ -139,23 +148,58 @@ void SpinnyCube::initializeGL()
 
 }
 
+
 void SpinnyCube::paintGL()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    double currentTime = time->elapsed() / 1000.0;
+    static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
+    static const GLfloat one = 1.0f;
+
+    glClearBufferfv(GL_COLOR, 0, green);
+    glClearBufferfv(GL_DEPTH, 0, &one);
+
     program->bind();
 
-    program->setUniformValueArray(proj_location, proj_matrix, 1);
+    glUniformMatrix4fv(proj_location, 1, GL_FALSE, proj_matrix->constData());
 
+#ifndef MANY_CUBES
+    int i;
+    for (i = 0; i < 24; i++)
+    {
+        float f = (float)i + (float)currentTime * 0.3f;
+        QMatrix4x4 mv_matrix;
+        mv_matrix.translate(0.0f, 0.0f, -6.0f);
+        mv_matrix.rotate((float)currentTime * 45.0f, 0.0f, 1.0f, 0.0f);
+        mv_matrix.rotate((float)currentTime * 21.0f, 1.0f, 0.0f, 0.0f);
+        mv_matrix.translate(sinf(2.1f * f) * 2.0f,
+                            cosf(1.7f * f) * 2.0f,
+                            sinf(1.3f * f) * cosf(1.5f * f) * 2.0f);
+        glUniformMatrix4fv(mv_location, 1, GL_FALSE, mv_matrix.constData());
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+#else
+    float f = (float)currentTime * 0.3f;
     QMatrix4x4 mv_matrix;
-    program->setUniformValueArray(mv_location, &mv_matrix, 1);
-
+    mv_matrix.translate(0.0f, 0.0f, -4.0f);
+    mv_matrix.translate(sinf(2.1f * f) * 0.5f,
+                        cosf(1.7f * f) * 0.5f,
+                        sinf(1.3f * f) * cosf(1.5f * f) * 2.0f);
+    mv_matrix.rotate((float)currentTime * 45.0f, 0.0f, 1.0f, 0.0f);
+    mv_matrix.rotate((float)currentTime * 81.0f, 1.0f, 0.0f, 0.0f);
+    glUniformMatrix4fv(mv_location, 1, GL_FALSE, mv_matrix.constData());
     glDrawArrays(GL_TRIANGLES, 0, 36);
+#endif
 }
 
 void SpinnyCube::resizeGL(int w, int h)
 {
     glViewport(0, 0, w, h);
     float aspect = static_cast<float>(w) / static_cast<float>(h);
-    *proj_matrix = QMatrix4x4();
+    proj_matrix->setToIdentity();
     proj_matrix->perspective(50.0f, aspect, 0.1f, 1000.0f);
+}
+
+void SpinnyCube::timerEvent(QTimerEvent *)
+{
+    updateGL();
 }
--- chapter05/spinnycube.h
+++ chapter05/spinnycube.h
@@ -2,10 +2,15 @@
 #define SPINNYCUBE_H
 
 #include <QGLWidget>
+#include <QOpenGLFunctions_4_3_Core>
+
 class QOpenGLShaderProgram;
 class QOpenGLVertexArrayObject;
 class QMatrix4x4;
-class SpinnyCube : public QGLWidget
+class QTimerEvent;
+class QTime;
+
+class SpinnyCube : public QGLWidget, protected QOpenGLFunctions_4_3_Core
 {
     Q_OBJECT
 public:
@@ -21,11 +26,24 @@ private:
     void paintGL();
     void resizeGL(int w, int h);
 
+    void timerEvent(QTimerEvent *);
+
     int mv_location,
         proj_location;
 
     QOpenGLShaderProgram* program;
     QMatrix4x4* proj_matrix;
+//    GLuint          program;
+    GLuint          vao;
+    GLuint          buffer;
+//    GLint           mv_location;
+//    GLint           proj_location;
+
+    float           aspect;
+
+    QTime* time;
+
+
 };
 
 #endif // SPINNYCUBE_H
